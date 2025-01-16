0b1efe2815af667ea56fdaaf7d2dc8642337de02
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2025-01-17 00:44:47 +1000

    restructure plugin configs

--- .config/nvim/lua/phillipe/keymaps.lua
+++ .config/nvim/lua/phillipe/keymaps.lua
@@ -1,6 +1,30 @@
+local keymap = vim.keymap
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
--- local key = vim.keymap
 
--- vim.keymap.set('n', '<leader>pf', "<CMD>Telescope find_files<CR>", {})
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+    callback = function(event)
+        -- Define mappings specfic for LSP related items
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
 
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[S]earch [D]ocument [S]ymbols')
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        map(']d', vim.diagnostic.goto_next, 'Goto Next [D]iagnostic')
+        map('[d', vim.diagnostic.goto_next, 'Goto Previous [D]iagnostic')
+    end
+})
