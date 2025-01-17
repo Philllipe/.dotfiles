0b022e0dbc050a1fe1201f0e45303e556a74b627
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2025-01-17 17:47:16 +1000

    replaced nvim-cmp with blink.cmp
    blink seems to be much more intutive than nvim-cmp. Less setup = less
    overhead

--- .config/nvim/lua/phillipe/plugins/lsp-config.lua
+++ .config/nvim/lua/phillipe/plugins/lsp-config.lua
@@ -1,37 +1,27 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		{ "j-hui/fidget.nvim", opts = {} },
-		{
-			"folke/lazydev.nvim",
-			ft = "lua",
-			opts = {
-				library = {
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
-	},
-	config = function()
-		local servers = {
-			-- lua_ls is configured through folke/lazydev
-		}
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "saghen/blink.cmp",
+        { "j-hui/fidget.nvim", opts = {} },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = { library = { { path = "${3rd}/luv/library", words = { "vim%.uv" } } } },
+        },
+    },
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			vim.lsp.protocol.make_client_capabilities(),
-			require("cmp_nvim_lsp").default_capabilities()
-		)
+        -- Example manual setup
+        -- lspconfig.lua_ls.setup({ capabilities = capabilities })
 
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				local server = servers[server_name] or {}
-				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-				require("lspconfig")[server_name].setup(server)
-			end,
-		})
-	end,
+        -- Automatic setup with mason
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({ capabilities = capabilities })
+            end,
+        })
+    end,
 }
