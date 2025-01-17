eaa066bffc9a7c516fe8d5eead6e6072d4cf1950
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2025-01-18 03:28:25 +1000

    formatted using spaces

--- .config/nvim/lua/phillipe/plugins/conform.lua
+++ .config/nvim/lua/phillipe/plugins/conform.lua
@@ -1,23 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "williamboman/mason.nvim" },
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true })
-			end,
-			mode = { "n", "v" },
-			desc = "[F]ormat buffer",
-		},
-	},
-	opts = {
-		format_on_save = { timeout_ms = 500 },
-		default_format_opts = { lsp_format = "fallback" },
-		formatters_by_ft = {
-			lua = { "stylua --indent-type Spaces" },
-		},
-	},
+    "stevearc/conform.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = { "n", "v" },
+            desc = "[F]ormat buffer",
+        },
+    },
+    opts = {
+        format_on_save = { timeout_ms = 500 },
+        default_format_opts = { lsp_format = "fallback" },
+        formatters_by_ft = {
+            lua = { "stylua --indent-type Spaces" },
+        },
+    },
 }
