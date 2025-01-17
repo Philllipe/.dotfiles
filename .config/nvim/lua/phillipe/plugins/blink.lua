d96a08d70797f5a733bb289837eb658ee9febfe8
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2025-01-18 02:44:52 +1000

    add LuaSnip & config for LazyDev

--- .config/nvim/lua/phillipe/plugins/blink.lua
+++ .config/nvim/lua/phillipe/plugins/blink.lua
@@ -1,21 +1,35 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+		},
+	},
 
 	version = "*",
 
 	opts = {
 		keymap = { preset = "default" },
+
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		snippets = { preset = "luasnip" },
+
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
 		},
 	},
-	opts_extend = { "sources.default" },
 }
